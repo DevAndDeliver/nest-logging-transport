c5474c8959e2f03f91934be88a165a7a647bbda1
Jakub <kubasurdej@gmail.com>
2021-09-07 22:11:10 +0200

    test: create e2e tests for error logging

--- test/logger.e2e-spec.ts
+++ test/logger.e2e-spec.ts
@@ -15,36 +15,80 @@ describe('Logger (e2e)', () => {
     let mockedApp: INestApplication;
     let mockedService: MockedService;
 
+    const mockedDate = new Date('1970');
+
     beforeAll(async () => {
         mockedApp = await NestFactory.create(ApplicationModule, {
             logger: new NestTransportLogger({ transports: [mockedTransport] }),
-            bufferLogs: true,
         });
         await mockedApp.init();
         mockedService = mockedApp.get<MockedService>(MockedService);
+
+        jest.useFakeTimers('modern');
+        jest.setSystemTime(mockedDate);
     });
 
     afterAll(async () => {
         mockedApp && (await mockedApp.close());
+        jest.useRealTimers();
     });
 
     afterEach(jest.resetAllMocks);
 
-    it('should add context to logs created using anonymous logger', () => {
+    it('should properly handle simple logs from anonymous logger', () => {
         const logString = 'simpleLog';
+
         for (const method of logMethods) {
             jest.resetAllMocks();
             mockedService.loggerWithoutContext[method](logString);
             expect(eventListener).toBeCalledTimes(1);
-            expect(eventListener).toBeCalledWith({});
+            expect(eventListener).toBeCalledWith({
+                baseContext: null,
+                additionalContext: null,
+                message: logString,
+                timestamp: mockedDate,
+                type: method,
+                stacktrace: null,
+            });
         }
+    });
+
+    it('should properly handle logs from logger with context', () => {
+        const logString = 'simpleLog';
+
+        for (const method of logMethods) {
+            jest.resetAllMocks();
+            mockedService.loggerWithContext[method](logString);
+            expect(eventListener).toBeCalledTimes(1);
+            expect(eventListener).toBeCalledWith({
+                baseContext: MockedService.name,
+                additionalContext: null,
+                message: logString,
+                timestamp: mockedDate,
+                type: method,
+                stacktrace: null,
+            });
+        }
+    });
+
+    it('should properly parse errors when error method is called', () => {
+        const testError = new Error();
+        mockedService.loggerWithContext.error(testError);
+
+        expect(eventListener).toBeCalledWith({
+            additionalContext: null,
+            baseContext: MockedService.name,
+            message: `${testError}`,
+            stacktrace: testError.stack,
+            timestamp: mockedDate,
+            type: 'error',
+        });
     });
 });
 
 const eventListener = jest.fn();
 const mockedTransport: Transport = {
     call(event) {
-        console.log('*************\n***********');
         eventListener(event);
     },
 };
