84404ba7d8dc2cdff8600a1210601df534f29e34
Jakub <kubasurdej@gmail.com>
2022-04-15 20:30:10 +0200

    feat: add JSON transport

--- src/main.ts
+++ src/main.ts
@@ -2,3 +2,4 @@ export * from './interfaces/transport';
 export * from './logger';
 
 export { DefaultTransportConsole } from './transports/defaultTransportConsole';
+export { TransportJson } from './transports/transportJson';
---
+++ src/transports/transportJson.ts
@@ -0,0 +1,16 @@
+import { LogEvent, Transport } from '@/interfaces/transport';
+
+export class TransportJson implements Transport {
+    public call(event: LogEvent): void {
+        const stringifiedEvent = this.stringify(event);
+        process.stdout.write(`${stringifiedEvent}\n`);
+    }
+
+    private stringify(event: LogEvent) {
+        try {
+            return JSON.stringify(event);
+        } catch (_e) {
+            return `${event}`;
+        }
+    }
+}
